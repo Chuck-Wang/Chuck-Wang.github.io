f0e0849c8e9b4040393f6af851b5ec6344efe2a7
Chuck Wang <Chuck.Wang@rice.edu>
2020-09-17 20:10:09 -0500

    pacwar site

---
+++ pacwar/script.js
@@ -0,0 +1,23 @@
+document.getElementById("submitBtn").onclick = function saveToFirebase() {
+
+    var patt = /[0123]{50}/
+    if (patt.test(document.getElementById("gene").value)) {
+        var messageObject = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            gene: document.getElementById("gene").value
+        };
+    
+        firebase.database().ref('web-messages').push().set(messageObject)
+            .then(function(snapshot) {
+                // success(); // some success method
+            }, function(error) {
+                console.log('error' + error);
+                error(); // some error method
+            });
+    } else {
+        document.getElementById("errorMsg").innerHTML = "Invalid gene!";
+    }
+}
+
+// saveToFirebase();
